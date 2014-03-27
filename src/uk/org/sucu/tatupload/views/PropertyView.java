efc2d36565b5afc7da30fc65870b7e362493f5a3
Alexander Lay <acl1g12@soton.ac.uk>
2014-03-27 17:00:16 +0000

    added some stuff

--- src/uk/org/sucu/tatupload/views/PropertyView.java
+++ src/uk/org/sucu/tatupload/views/PropertyView.java
@@ -1,5 +1,29 @@
 package uk.org.sucu.tatupload.views;
 
-public class PropertyView {
+import android.content.Context;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+public class PropertyView extends LinearLayout{
+	
+	private String text;
+	private TextView tv;
+	private EditText edt;
+	private Button button;
+	
+	public PropertyView(String str, Context context) {
+		super(context);
+		text = str;
+		tv = new TextView(context);
+		edt = new EditText(context);
+		button = new Button(context);
+		
+		tv.setText(text);
+		button.setText("Edit");
+	}
+
+	
+	
 }
