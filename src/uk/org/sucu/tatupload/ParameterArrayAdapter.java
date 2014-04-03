e0d4ed19926550ac1af977d843830914313d2155
Alexander Lay <acl1g12@soton.ac.uk>
2014-04-03 23:29:55 +0100

    Created adapter for parameter arraylist to textviews

---
+++ src/uk/org/sucu/tatupload/ParameterArrayAdapter.java
@@ -0,0 +1,25 @@
+package uk.org.sucu.tatupload;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class ParameterArrayAdapter extends ArrayAdapter<String> {
+
+	public ParameterArrayAdapter(Context context, int resource,
+			List<String> objects) {
+		super(context, resource, objects);
+		// TODO Auto-generated constructor stub
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent){
+		TextView tv = new TextView(getContext());
+		tv.setText(getItem(position));
+		return tv;
+	}
+	
+}
