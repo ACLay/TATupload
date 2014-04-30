df60cab6f77f932ce7d791123a5fb06ff2726649
Alexander Lay <acl1g12@soton.ac.uk>
2014-04-30 19:37:16 +0100

    Added an item to restore the settings to their default.

--- src/uk/org/sucu/tatupload/OptionActivity.java
+++ src/uk/org/sucu/tatupload/OptionActivity.java
@@ -1,17 +1,50 @@
 package uk.org.sucu.tatupload;
 
+import uk.org.sucu.tatupload.parse.Parameters;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 
+@SuppressWarnings("deprecation") //it uses methods replaced within new PreferenceFragments in honeycomb
 public class OptionActivity extends PreferenceActivity {
 
-	@SuppressWarnings("deprecation")
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-        
-        
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+
+
+	}
+
+	public void onStart(){
+		super.onStart();
+		//TODO only allow restore to default when processing is turned off.
+		final int ver_seen = SettingsAccessor.getTutorialVersionSeen(this);
+
+		final PreferenceActivity c = this;
+
+		Preference restore = findPreference(getString(R.string.restore_defaults));
+		restore.setOnPreferenceClickListener(new OnPreferenceClickListener(){
+			@Override
+			public boolean onPreferenceClick(Preference arg0) {
+				SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(c);
+				//clear the user settings from the file, keeping the tutorial version seen
+				sharedPref.edit()
+					.clear()
+					.putInt(getString(R.string.tutorial_ver_key), ver_seen)
+					.commit();
+				Parameters.restoreDefaults();//update the values kept in memory
+				//remake the settings screen so the new values are shown
+				c.finish();
+				c.startActivity(c.getIntent());
+				return true;
+			}
+
+		});
+
+	}
 
 }
