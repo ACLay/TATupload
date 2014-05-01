f3f7ae54c42a0e4fa5538d3baee2caa98eb49e69
Alexander Lay <acl1g12@soton.ac.uk>
2014-05-01 16:45:45 +0100

    new methods for reseting parameters

--- src/uk/org/sucu/tatupload/OptionActivity.java
+++ src/uk/org/sucu/tatupload/OptionActivity.java
@@ -3,48 +3,51 @@ package uk.org.sucu.tatupload;
 import uk.org.sucu.tatupload.parse.Parameters;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
-import android.preference.PreferenceManager;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 @SuppressWarnings("deprecation") //it uses methods replaced within new PreferenceFragments in honeycomb
 public class OptionActivity extends PreferenceActivity {
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
-
-
-	}
-
-	public void onStart(){
-		super.onStart();
-		//TODO only allow restore to default when processing is turned off.
-		final int ver_seen = SettingsAccessor.getTutorialVersionSeen(this);
-
-		final PreferenceActivity c = this;
-
+		
 		Preference restore = findPreference(getString(R.string.restore_defaults));
 		restore.setOnPreferenceClickListener(new OnPreferenceClickListener(){
 			@Override
 			public boolean onPreferenceClick(Preference arg0) {
-				SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(c);
-				//clear the user settings from the file, keeping the tutorial version seen
-				sharedPref.edit()
-					.clear()
-					.putInt(getString(R.string.tutorial_ver_key), ver_seen)
-					.commit();
-				Parameters.restoreDefaults();//update the values kept in memory
-				//remake the settings screen so the new values are shown
-				c.finish();
-				c.startActivity(c.getIntent());
+				resetParameters();
+				updateView();
 				return true;
 			}
 
 		});
-
 	}
+	
+	private void resetParameters(){
+		
+		int ver_seen = SettingsAccessor.getTutorialVersionSeen(this);
+		boolean processing = SettingsAccessor.getProcessingTexts(this);
 
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+		//clear the user settings from the file, keeping the tutorial version seen
+		sharedPref.edit()
+			.clear()
+			.putBoolean(getString(R.string.processing_key), processing)
+			.putInt(getString(R.string.tutorial_ver_key), ver_seen)
+			.commit();
+		Parameters.restoreDefaults();//update the values kept in memory
+	}
+	
+	//reload the parameters into the view.
+	private void updateView(){
+		CheckBoxPreference autoQueue = (CheckBoxPreference) findPreference(getString(R.string.confirm_split_key));
+		autoQueue.setChecked(SettingsAccessor.getAutoQueueTexts(this));
+	}
+	
 }
