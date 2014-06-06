3802964ddf17a9b1bb12b3c579aa130d7bcfce99
Alexander Lay <acl1g12@soton.ac.uk>
2014-06-06 11:38:15 +0100

    Fixed a copy-paste error in makeAddPopup()

--- src/uk/org/sucu/tatupload/ParameterViewActivity.java
+++ src/uk/org/sucu/tatupload/ParameterViewActivity.java
@@ -249,7 +249,7 @@ public class ParameterViewActivity extends Activity {
 		remove.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v) {
-				removeParameter();
+				addParameter();
 			}
 		});
 		
