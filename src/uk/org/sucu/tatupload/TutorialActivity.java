a5ede1e18be101428169d897919ea4971aa487d8
Alexander Lay <acl1g12@soton.ac.uk>
2014-06-09 14:06:09 +0100

    Created a new activity for the tutorial, with quick links to setup parameters.

---
+++ src/uk/org/sucu/tatupload/TutorialActivity.java
@@ -0,0 +1,31 @@
+package uk.org.sucu.tatupload;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TutorialActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tutorial);
+	}
+	
+	public void setupFlavours(View v){
+		Intent intent = new Intent(this, ParameterViewActivity.class);
+		intent.putExtra(getString(R.string.parameter_indicator), getString(R.string.flavour_parameter));
+		startActivity(intent);
+	}
+	
+	public void setupLocation(View v){
+		Intent intent = new Intent(this, ParameterViewActivity.class);
+		intent.putExtra(getString(R.string.parameter_indicator), getString(R.string.location_parameter));
+		startActivity(intent);
+	}
+	
+	public void done(View v){
+		this.finish();
+	}
+}
