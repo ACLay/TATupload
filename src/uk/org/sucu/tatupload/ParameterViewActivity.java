9b830844947db6da52387d65df706b410015a48c
Alexander Lay <acl1g12@soton.ac.uk>
2014-07-01 19:14:23 +0100

    Changed Popups to AlertDialogs

--- src/uk/org/sucu/tatupload/ParameterViewActivity.java
+++ src/uk/org/sucu/tatupload/ParameterViewActivity.java
@@ -8,21 +8,17 @@ import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.PopupWindow;
-import android.widget.PopupWindow.OnDismissListener;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -32,7 +28,7 @@ public class ParameterViewActivity extends Activity {
 	private ParameterArrayAdapter adapter;
 	private String parameterIdentifier;
 	
-	private PopupWindow popup;
+	private AlertDialog dialog;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -109,62 +105,46 @@ public class ParameterViewActivity extends Activity {
 	}
 
 	public void openAddDialogue(View v){
-		popup = makeAddPopup();
-		openPopup();
+		makeAddPopup();
 	}
 	public void openEditDialogue(View v){
-		popup = makeEditPopup();
-		openPopup();
+		makeEditPopup();
 	}
 	public void openRemoveDialogue(View v){
-		popup = makeRemovalPopup();
-		openPopup();
+		makeRemovalPopup();
 	}
-	
-	private void openPopup(){
-		popup.setOnDismissListener(new OnDismissListener() {
-			@Override
-			public void onDismiss() {
-				adapter.notifyDataSetChanged();
-				saveParameter();
-			}
-		});
-		popup.showAtLocation(findViewById(R.id.parameterLayout), Gravity.CENTER, 0, 0);
-	}
-	
-	private void closePopup(){
-		popup.dismiss();
-	}
+
 	
 	@SuppressLint("DefaultLocale")
 	public void addParameter(){
-		EditText textbox = (EditText) popup.getContentView().findViewById(R.id.addParamTextField);
+		
+		EditText textbox = (EditText) dialog.findViewById(R.id.addParamTextField);
 		String param = textbox.getText().toString().toLowerCase();
 		
 		if(!parameter.contains(param)){
 			parameter.add(param);
+			adapter.notifyDataSetChanged();
 		}
-		closePopup();
 	}
 
 	@SuppressLint("DefaultLocale")
 	public void editParameter(){
-		Spinner spin = (Spinner) popup.getContentView().findViewById(R.id.editSpinner);
+		Spinner spin = (Spinner) dialog.findViewById(R.id.editSpinner);
 		String toChange = (String) spin.getSelectedItem();
-		EditText textbox = (EditText) popup.getContentView().findViewById(R.id.editParamTextField);
+		EditText textbox = (EditText) dialog.findViewById(R.id.editParamTextField);
 		
 		int index = parameter.indexOf(toChange);
 		if(index != -1){//indexOf returns -1 if the object isn't present
 			parameter.set(index, textbox.getText().toString().toLowerCase());
+			adapter.notifyDataSetChanged();
 		}
-		closePopup();
 	}
 	
 	public void removeParameter(){
-		Spinner spin = (Spinner) popup.getContentView().findViewById(R.id.removeSpinner);
+		Spinner spin = (Spinner) dialog.findViewById(R.id.removeSpinner);
 		String toTake = (String) spin.getSelectedItem();
 		parameter.remove(toTake);
-		closePopup();
+		adapter.notifyDataSetChanged();
 	}
 	
 	private void saveParameter(){
@@ -177,91 +157,73 @@ public class ParameterViewActivity extends Activity {
 						.setMessage("Unable to save the parameter.")
 						.setPositiveButton("Okay", null)  
 						.setCancelable(false)  
-						.create()  
+						.create()
 						.show();
 			}
 		}
 	}
 	
-	private PopupWindow makeRemovalPopup(){
-		
+	private void makeRemovalPopup(){
+		//build the dialog box
 		View viewToLoad = LayoutInflater.from(this).inflate(R.layout.remove_param_popup, null);		
-		PopupWindow popup = new PopupWindow(viewToLoad, 200, 150, true);
+		dialog = new AlertDialog.Builder(this)
+			.setView(viewToLoad)
+			.setPositiveButton(R.string.remove_param, new DialogInterface.OnClickListener(){
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					removeParameter();
+				}
+			})
+			.setNegativeButton(R.string.cancel, null)
+			.create();
+		//connect the dialogs spinner to the data structure
 		Spinner spin = (Spinner) viewToLoad.findViewById(R.id.removeSpinner);
-		
 		@SuppressWarnings({ "rawtypes", "unchecked" })
 		ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,parameter);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spin.setAdapter(adapter);
-		
-		Button cancel = (Button) viewToLoad.findViewById(R.id.cancelRemove);
-		cancel.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				closePopup();
-			}
-		});
-		
-		Button remove = (Button) viewToLoad.findViewById(R.id.removeButton);
-		remove.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				removeParameter();
-			}
-		});
-		
-		return popup;
+		//display the dialog
+		dialog.show();
 	}
 
-	private PopupWindow makeEditPopup(){
-		View viewToLoad = LayoutInflater.from(this).inflate(R.layout.edit_param_popup, null);		
-		PopupWindow popup = new PopupWindow(viewToLoad, 200, 300, true);
+	private void makeEditPopup(){
+		View viewToLoad = LayoutInflater.from(this).inflate(R.layout.edit_param_popup, null);	
+		//build the dialog box
+		dialog = new AlertDialog.Builder(this)
+			.setView(viewToLoad)
+			.setPositiveButton(R.string.change_param, new DialogInterface.OnClickListener(){
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					editParameter();
+				}
+			})
+			.setNegativeButton(R.string.cancel, null)
+			.create();
+		//connect its spinner to the data structure
 		Spinner spin = (Spinner) viewToLoad.findViewById(R.id.editSpinner);
-		
 		@SuppressWarnings({ "rawtypes", "unchecked" })
 		ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,parameter);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spin.setAdapter(adapter);
+		//display the dialog
+		dialog.show();
 		
-		Button cancel = (Button) viewToLoad.findViewById(R.id.cancelEdit);
-		cancel.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				closePopup();
-			}
-		});
-		
-		Button remove = (Button) viewToLoad.findViewById(R.id.editButton);
-		remove.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				editParameter();
-			}
-		});
-		
-		return popup;
 	}
 	
-	private PopupWindow makeAddPopup(){
-		View viewToLoad = LayoutInflater.from(this).inflate(R.layout.add_param_popup, null);		
-		PopupWindow popup = new PopupWindow(viewToLoad, 200, 150, true);
+	private void makeAddPopup(){
+		View viewToLoad = LayoutInflater.from(this).inflate(R.layout.add_param_popup, null);	
 		
-		Button cancel = (Button) viewToLoad.findViewById(R.id.cancelAdd);
-		cancel.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				closePopup();
-			}
-		});
+		dialog = new AlertDialog.Builder(this)
+			.setView(viewToLoad)
+			.setPositiveButton(R.string.add_param, new DialogInterface.OnClickListener(){
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					addParameter();
+				}
+			})
+			.setNegativeButton(R.string.cancel, null)
+			.create();
+		dialog.show();
 		
-		Button remove = (Button) viewToLoad.findViewById(R.id.addButton);
-		remove.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				addParameter();
-			}
-		});
-		
-		return popup;
 	}
 }
