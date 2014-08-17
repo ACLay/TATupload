70e5d90c6b686cdf4cd551edccc368ddd9df082b
Alexander Lay <acl1g12@soton.ac.uk>
2014-08-17 19:38:03 +0100

    Removed preference mutators, which were moved into Settings

--- src/uk/org/sucu/tatupload/TatUploadApplication.java
+++ src/uk/org/sucu/tatupload/TatUploadApplication.java
@@ -2,52 +2,10 @@ package uk.org.sucu.tatupload;
 
 import java.util.ArrayList;
 import uk.org.sucu.tatupload.parse.Parameters;
-import android.annotation.SuppressLint;
 import android.app.Application;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
 public class TatUploadApplication extends Application {
 	
-	//TODO move settings into Settings class
-	public void setProcessingTexts(boolean processingTexts){
-		getEditor()
-		.putBoolean(getString(R.string.processing_key), processingTexts)
-		.commit();
-	}
-
-	public void setConfirmSplit(boolean confirmSplit){
-		getEditor()
-		.putBoolean(getString(R.string.confirm_split_key), confirmSplit)
-		.commit();
-	}
-	
-	public void setTutorialVersionShown(int version){
-		getEditor()
-		.putInt(getString(R.string.tutorial_ver_key), version)
-		.commit();
-	}
-	
-	public void setBrowserData(String packageName, String name){
-		getEditor()
-		.putString(getString(R.string.browser_package_key), packageName)
-		.putString(getString(R.string.browser_name_key), name)
-		.commit();
-	}
-	
-	public void removePreference(String key){
-		getEditor()
-		.remove(key)
-		.commit();
-	}
-	
-	
-	@SuppressLint("CommitPrefEdits")
-	private SharedPreferences.Editor getEditor(){
-		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-		SharedPreferences.Editor editor = sharedPref.edit();
-		return editor;
-	}
 	
 	public void onCreate(){
 		super.onCreate();
