8b1effb8b45e640237a0e319badc144f2cf2adaf
Alexander Lay <acl1g12@soton.ac.uk>
2014-08-17 16:14:36 +0100

    Refactoring of settings mutators and removal of unused variables.

--- src/uk/org/sucu/tatupload/TatUploadApplication.java
+++ src/uk/org/sucu/tatupload/TatUploadApplication.java
@@ -9,51 +9,32 @@ import android.preference.PreferenceManager;
 
 public class TatUploadApplication extends Application {
 	
-	private static boolean processingTexts = false;
-	private static boolean confirmSplit = false;
-	//private static ArrayList<Text> messages = new ArrayList<Text>();
-	private static String formName = "";
-	//private static final String uploadScript = "https://script.google.com/macros/s/AKfycbzfPd5U7tbyOmK8EERxB8LPn53CzLy_nzXzAu2jb2_fYC8V_aof/exec";
-
-	private static int tutorialVersionShown;
-	//TODO update this code, so much legacy garbage!!!
 	//TODO move settings into SettingsAccessor class
-	public void setProcessingTexts(boolean b){
-		processingTexts = b;
-		//TODO is there a better way to do this?
-		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-		SharedPreferences.Editor editor = sharedPref.edit();
-		editor.putBoolean(getString(R.string.processing_key), processingTexts);
-		editor.commit();
+	public void setProcessingTexts(boolean processingTexts){
+		getEditor()
+		.putBoolean(getString(R.string.processing_key), processingTexts)
+		.commit();
 	}
 
-	public void setConfirmSplit(boolean b){
-		confirmSplit = b;
-		
-		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-		SharedPreferences.Editor editor = sharedPref.edit();
-		editor.putBoolean(getString(R.string.confirm_split_key), confirmSplit);
-		editor.commit();
+	public void setConfirmSplit(boolean confirmSplit){
+		getEditor()
+		.putBoolean(getString(R.string.confirm_split_key), confirmSplit)
+		.commit();
 	}
+	
 	//TODO this is no longer used, and should be removed from existing installations
-	public void setFormName(String newName){
-		formName= newName;
-		
-		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-		SharedPreferences.Editor editor = sharedPref.edit();
-		editor.putString(getString(R.string.form_name_key), formName);
-		editor.commit();
+	public void setFormName(String formName){
+		getEditor()
+		.putString(getString(R.string.form_name_key), formName)
+		.commit();
 	}
 	
 	public void setTutorialVersionShown(int version){
-		tutorialVersionShown = version;
-		
-		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-		SharedPreferences.Editor editor = sharedPref.edit();
-		editor.putInt(getString(R.string.tutorial_ver_key), tutorialVersionShown);
-		editor.commit();
+		getEditor()
+		.putInt(getString(R.string.tutorial_ver_key), version)
+		.commit();
 	}
-	//TODO use this as the new model for setters
+	
 	public void setBrowserData(String packageName, String name){
 		getEditor()
 		.putString(getString(R.string.browser_package_key), packageName)
