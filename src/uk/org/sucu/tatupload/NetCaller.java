f9656180a8bf860adc8a7f0ceafddd1296999ee4
Alexander Lay <acl1g12@soton.ac.uk>
2014-08-17 15:52:16 +0100

    Removed second script call method used in browser test from previous browser select code

--- src/uk/org/sucu/tatupload/NetCaller.java
+++ src/uk/org/sucu/tatupload/NetCaller.java
@@ -26,17 +26,6 @@ public class NetCaller {
 	    //start the browser
 	    context.startActivity(browserIntent);
 	}
-	
-	public static void callScriptWithChoice(Uri uri, Context context){
-
-		//While it would be nice to handle everything in-app, it seems for the time being I'll need to go via the browser.
-		Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
-		//open all this apps requests in the same tab, prevents new ones with each call
-		browserIntent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
-		//allows a new task to be started outside of a current task
-		browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		context.startActivity(browserIntent);
-	}
 
 	public static boolean isOnline(Context context){
 
