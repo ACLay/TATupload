42a3a90a4e423c1caf20f537cb2ce9c7967f73f4
Alexander Lay <acl1g12@soton.ac.uk>
2014-08-25 14:59:41 +0100

    Added extra setup choices, moved setup into Dialogs

--- src/uk/org/sucu/tatupload/activity/MainActivity.java
+++ src/uk/org/sucu/tatupload/activity/MainActivity.java
@@ -2,23 +2,31 @@ package uk.org.sucu.tatupload.activity;
 
 import java.util.Collection;
 
+import uk.org.sucu.tatupload.BrowserAccessor;
 import uk.org.sucu.tatupload.MessageArrayAdapter;
+import uk.org.sucu.tatupload.NetCaller;
 import uk.org.sucu.tatupload.R;
 import uk.org.sucu.tatupload.Settings;
 import uk.org.sucu.tatupload.message.SmsList;
 import uk.org.sucu.tatupload.message.Text;
+import uk.org.sucu.tatupload.parse.Parser;
 import uk.org.sucu.tatupload.views.QueuedSmsView;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 
 
 public class MainActivity extends Activity {
@@ -45,6 +53,7 @@ public class MainActivity extends Activity {
 		} else if(versionSeen < 4){// add extra cases inform the user of changes to the app, etc.
 			//remove the now unused form name field from the preference save file.
 			Settings.removePreference(getString(R.string.form_name_key),this);
+			BrowserAccessor.openBrowserChoicePopup(this, false);
 		}
 		if(versionSeen != TUTORIAL_VERSION){
 			Settings.setTutorialVersionShown(TUTORIAL_VERSION, this);
@@ -55,8 +64,16 @@ public class MainActivity extends Activity {
 
 	private void setupUI(){
 
-		if(Settings.getProcessingTexts(this)){
+		if(Settings.getUsed(this)){
 			setContentView(R.layout.message_queue);
+			
+			Button toggleButton = (Button) findViewById(R.id.toggleTatButton);
+			if(Settings.getProcessingTexts(this)){
+				toggleButton.setText(R.string.stop);
+			} else {
+				toggleButton.setText(R.string.start);
+			}
+			
 			ListView messageView = (ListView) findViewById(R.id.messageListView);
 			adapter = SmsList.getMessageArrayAdapter(this);
 			messageView.setAdapter(adapter);
@@ -111,22 +128,121 @@ public class MainActivity extends Activity {
 	}
 
 	public void startTat(View v){
-		Intent intent = new Intent(this, SetupActivity.class);
-		startActivity(intent);
-		finish();
+		startTatNewSpreadsheet();
 	}
 	
-	public void stopTat(View v){
-		DialogInterface.OnClickListener action = new DialogInterface.OnClickListener(){
+	//TODO test these start methods, implement new webscript methods
+	private void resumeTat(){
+		Settings.setProcessingTexts(true, this);
+		Button toggleButton = (Button) findViewById(R.id.toggleTatButton);
+		toggleButton.setText(R.string.stop);
+	}
+	
+	private void startTatNewSheet(){
+		if(NetCaller.isOnlineWithToast(this)){
+			Uri uri = Parser.createAddSheetUri(this,"Texts");
+			NetCaller.callScript(uri, this);
+			resumeTat();
+		} else {
+			openStartTatDialog();
+		}
+	}
+	
+	private void startTatNewSpreadsheet(){
+		startTatNewSpreadsheet(getString(R.string.text_a_toastie) + " " + Parser.getCurrentDateString());
+	}
+	
+	private void startTatNewSpreadsheet(String defaultName){
+		View view = LayoutInflater.from(this).inflate(R.layout.setup_layout , null);
+		final EditText ssNameEdit = (EditText) view.findViewById(R.id.ssNameEditText);
+		
+		ssNameEdit.setText(defaultName);
+				
+		new AlertDialog.Builder(this)
+		.setTitle(R.string.start)
+		.setView(view)
+		.setPositiveButton(R.string.start, new DialogInterface.OnClickListener() {
+			
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				//stop processing
-				Settings.setProcessingTexts(false, MainActivity.this);
-				//rebuild UI
-				setupUI();
+				
+				String ssName = ssNameEdit.getText().toString();
+					
+				if(NetCaller.isOnlineWithToast(MainActivity.this)){
+						
+					Uri uri = Parser.createNewSpreadsheetUri(ssName, MainActivity.this);
+					NetCaller.callScript(uri, MainActivity.this);
+					
+					if(!Settings.getUsed(MainActivity.this)){
+						Settings.setUsed(true, MainActivity.this);
+						setupUI();//the toggle button must be created before resumeTat() sets its text
+					}
+					
+					resumeTat();
+					
+				} else {
+					startTatNewSpreadsheet(ssName);
+				}
+				
+
 			}
-		};
-		confirmChoice(action, R.string.stop);
+		})
+		.setNegativeButton(android.R.string.cancel, null)
+		.create()
+		.show();
+	}
+	
+	private void stopTat(){
+		Settings.setProcessingTexts(false, this);
+		Button toggleButton = (Button) findViewById(R.id.toggleTatButton);
+		toggleButton.setText(R.string.start);
+	}
+	
+	public void toggleTat(View v){
+		//if TAT is running
+		if(Settings.getProcessingTexts(this)){
+			DialogInterface.OnClickListener positiveAction = new DialogInterface.OnClickListener(){
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					//stop processing new texts
+					stopTat();
+				}
+			};
+			confirmChoice(positiveAction, R.string.stop);
+		} else {
+			openStartTatDialog();
+		}
+	}
+	
+	public void openStartTatDialog(){
+		final RadioGroup v = (RadioGroup) LayoutInflater.from(this).inflate(R.layout.start_method_chooser , null);;
+		//give choice to start in same sheet, new sheet, or new spreadsheet
+		new AlertDialog.Builder(this)
+		.setTitle(R.string.start)
+		.setView(v)
+	
+		.setPositiveButton(R.string.start, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+				int selected = v.getCheckedRadioButtonId();
+				
+				switch(selected){
+				case R.id.radio_new_ss:
+					startTatNewSpreadsheet();
+					break;
+				/*case R.id.radio_new_sheet:
+					startTatNewSheet();
+					break;*/ //TODO add this
+				case R.id.radio_resume:
+					resumeTat();
+					break;
+				}
+			}
+		})
+		.setNegativeButton(android.R.string.cancel, null)
+		.create()
+		.show();
 	}
 	
 	public void showSettings(View v){
