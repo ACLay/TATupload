193b33055f329d4ee4a91242419da31580090bfc
Alexander Lay <acl1g12@soton.ac.uk>
2014-08-27 03:51:42 +0100

    Added app icons to the browser choice popup's spinner

--- src/uk/org/sucu/tatupload/ResolveInfoArrayAdapter.java
+++ src/uk/org/sucu/tatupload/ResolveInfoArrayAdapter.java
@@ -1,14 +1,17 @@
 package uk.org.sucu.tatupload;
 
 import java.util.List;
-
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ResolveInfoArrayAdapter extends ArrayAdapter<ResolveInfo> {
@@ -26,14 +29,25 @@ public class ResolveInfoArrayAdapter extends ArrayAdapter<ResolveInfo> {
 		return makeView(position);
 	}
 	
+	@SuppressLint("InflateParams")
 	private View makeView(int position){
 		
-		View view = LayoutInflater.from(getContext()).inflate(R.layout.browser_data_layout, null);
-		TextView tv = (TextView) view.findViewById(R.id.browserNameLabel);
 		ResolveInfo info = getItem(position);
 
+		View view = LayoutInflater.from(getContext()).inflate(R.layout.browser_data_layout, null);
+		
+		TextView tv = (TextView) view.findViewById(R.id.browserNameLabel);
 		tv.setText(info.loadLabel(pm));
-
+		
+		ImageView iv = (ImageView) view.findViewById(R.id.iconImageView);//your package name
+		Drawable icon;
+		try {
+			icon = getContext().getPackageManager().getApplicationIcon(info.activityInfo.packageName);
+			iv.setImageDrawable(icon);
+		} catch (NameNotFoundException e) {
+			
+		}
+		
 		return view;
 	}
 	
